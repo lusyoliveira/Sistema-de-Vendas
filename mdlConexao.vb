aae68529de116b427bffe4598709a17d6c543634
lusyoliveira <122019040+lusyoliveira@users.noreply.github.com>
2024-06-24 14:50:20 -0300

    Alterado string de conexão

--- mdlConexao.vb
+++ mdlConexao.vb
@@ -1,51 +1,133 @@
 Module mdlConexao
-    Private aConexao As New ADODB.Connection
+Imports System.Data.Common
+Imports System.Configuration
 
-    Const BancoDeDados = "dbVendas"
+    Private cn As DbConnection
 
-    Const acess_db = "bancodedados"
-    Const acess_usr = "usuario"
-    Const acess_pwd = "senha"
-    Const acess_server = "(local)"
+    Const DataBase = "dbVendas"
+    Const user = "usuario"
+    Const password = "senha"
+    Const Servidor = "(local)"
 
     Public Enum tpServidor
         Access = 0
         SqlServer = 1
+        MySQL = 2
+        PostgreSQL = 3
+        Oracle = 4
+        SQLite = 5
     End Enum
 
-    Public Function RecebeTabela(ByVal Sql As String, Optional ByVal servidor As tpServidor = tpServidor.Access)
+    Private Function GetConnectionString(ByVal servidor As tpServidor) As String
+        Select Case servidor
+            Case tpServidor.SqlServer
+                Return $"Server={Servidor};Database={DataBase};User Id={user};Password={password};"
+            Case tpServidor.MySQL
+                Return $"Server={Servidor};Database={DataBase};User={user};Password={password};"
+            Case tpServidor.PostgreSQL
+                Return $"Host={Servidor};Database={DataBase};Username={user};Password={password};"
+            Case tpServidor.Oracle
+                Return $"Data Source={Servidor};User Id={user};Password={password};"
+            Case tpServidor.SQLite
+                Dim dbPath As String = My.Computer.FileSystem.CurrentDirectory & If(Right(My.Computer.FileSystem.CurrentDirectory, 1) = "\", "", "\") & BancoDeDados & ".sqlite"
+                Return $"Data Source={dbPath};Version=3;"
+            Case Else ' Access
+                Dim acess_db2 As String = My.Computer.FileSystem.CurrentDirectory
+                acess_db2 &= If(Right(acess_db2, 1) = "\", "", "\")
+                acess_db2 &= DataBase & ".mdb"
+                Return $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={DataBase};Persist Security Info=False"
+        End Select
+    End Function
 
-        Dim aResultado As New ADODB.Recordset
-        Dim acess_db2 As String
-        acess_db2 = My.Computer.FileSystem.CurrentDirectory
-        acess_db2 &= IIf(Right(acess_db2, 1) = "\", "", "\")
-        acess_db2 &= BancoDeDados & ".mdb"
-        'MsgBox(acess_db2)
-        If aConexao.State = 1 Then aConexao.Close()
+    Private Function GetProviderInvariantName(ByVal servidor As tpServidor) As String
+        Select Case servidor
+            Case tpServidor.SqlServer
+                Return "System.Data.SqlClient"
+            Case tpServidor.MySQL
+                Return "MySql.Data.MySqlClient"
+            Case tpServidor.PostgreSQL
+                Return "Npgsql"
+            Case tpServidor.Oracle
+                Return "Oracle.ManagedDataAccess.Client"
+            Case tpServidor.SQLite
+                Return "System.Data.SQLite"
+            Case Else ' Access
+                Return "System.Data.OleDb"
+        End Select
+    End Function
 
-        If servidor = tpServidor.SqlServer Then
+    Public Function RecebeTabela(ByVal Sql As String, Optional ByVal servidor As tpServidor = tpServidor.Access) As DataTable
+        Dim factory As DbProviderFactory = DbProviderFactories.GetFactory(GetProviderInvariantName(servidor))
+        cn = factory.CreateConnection()
+        cn.ConnectionString = GetConnectionString(servidor)
 
-            aConexao.ConnectionString = "driver={sql server};" & _
-                                            "server=" + acess_server + ";" & _
-                                            "Database=" + acess_db + ";" & _
-                                            "PWD=" + acess_pwd + ";" & _
-                                            "UID=" + acess_usr + ";"
-        Else
-            aConexao.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & acess_db2 & ";Persist Security Info=False"
-        End If
+        Dim dt As New DataTable()
         Try
-            aConexao.Open()
-
+            cn.Open()
+            Dim cmd As DbCommand = aConexao.CreateCommand()
+            cmd.CommandText = Sql
+            Dim adapter As DbDataAdapter = factory.CreateDataAdapter()
+            adapter.SelectCommand = cmd
+            adapter.Fill(dt)
         Catch ex As Exception
-            aResultado = Nothing
-            MsgBox("Banco de Dados não encontrado!")
-            GoTo fim
+            dt = Nothing
+            MsgBox("Banco de Dados nÃ£o encontrado!")
+        Finally
+            If cn.State = ConnectionState.Open Then
+                cn.Close()
+            End If
         End Try
 
-        aResultado.CursorLocation = ADODB.CursorLocationEnum.adUseClient
-        aResultado.Open(Sql, aConexao, ADODB.CursorTypeEnum.adOpenDynamic, ADODB.LockTypeEnum.adLockOptimistic)
-fim:
-        RecebeTabela = aResultado
-        aResultado = Nothing
+        Return dt
     End Function
+
+   '    Private aConexao As New ADODB.Connection
+
+'    Const BancoDeDados = "dbVendas"
+
+'    Const acess_db = "bancodedados"
+'    Const acess_usr = "usuario"
+'    Const acess_pwd = "senha"
+'    Const acess_server = "(local)"
+
+'    Public Enum tpServidor
+'        Access = 0
+'        SqlServer = 1
+'    End Enum
+
+'    Public Function RecebeTabela(ByVal Sql As String, Optional ByVal servidor As tpServidor = tpServidor.Access)
+
+'        Dim aResultado As New ADODB.Recordset
+'        Dim acess_db2 As String
+'        acess_db2 = My.Computer.FileSystem.CurrentDirectory
+'        acess_db2 &= IIf(Right(acess_db2, 1) = "\", "", "\")
+'        acess_db2 &= BancoDeDados & ".mdb"
+'        'MsgBox(acess_db2)
+'        If aConexao.State = 1 Then aConexao.Close()
+
+'        If servidor = tpServidor.SqlServer Then
+
+'            aConexao.ConnectionString = "driver={sql server};" &
+'                                            "server=" + acess_server + ";" &
+'                                            "Database=" + acess_db + ";" &
+'                                            "PWD=" + acess_pwd + ";" &
+'                                            "UID=" + acess_usr + ";"
+'        Else
+'            aConexao.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & acess_db2 & ";Persist Security Info=False"
+'        End If
+'        Try
+'            aConexao.Open()
+
+'        Catch ex As Exception
+'            aResultado = Nothing
+'            MsgBox("Banco de Dados nï¿½o encontrado!")
+'            GoTo fim
+'        End Try
+
+'        aResultado.CursorLocation = ADODB.CursorLocationEnum.adUseClient
+'        aResultado.Open(Sql, aConexao, ADODB.CursorTypeEnum.adOpenDynamic, ADODB.LockTypeEnum.adLockOptimistic)
+'fim:
+'        RecebeTabela = aResultado
+'        aResultado = Nothing
+'    End Function
 End Module
